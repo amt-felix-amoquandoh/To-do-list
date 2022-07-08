95d7b1598623113303e5cf6b5ff3c4fd55f76a26
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-08 22:11:42 +0000

    first commit

---
+++ js/sript.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('addButton'); 
+var userInput = document.getElementById('userInput');
+
+button.addEventListener('click', function() {
+    console.log('mims');
+  });
