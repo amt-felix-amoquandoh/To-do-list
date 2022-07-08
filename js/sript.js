b795b811ecc7116ee28f4c8356405478af674669
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-08 22:54:28 +0000

    second commit

--- js/sript.js
+++ js/sript.js
@@ -3,7 +3,21 @@ var userInput = document.getElementById('userInput');
 var listItem = document.getElementById('listItem');
 
 button.addEventListener('click', function() {
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(''));
-    listItem.appendChild(li);
+    if(userInput.value.length > 0){
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(userInput.value));
+        listItem.appendChild(li);
+        userInput.value = '';
+    }
+   
+  });
+
+  userInput.addEventListener('keypress', function(event) {
+    if(userInput.value.length > 0 && event.keyCode === 13 ){
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(userInput.value));
+        listItem.appendChild(li);
+        userInput.value = '';
+    }
+   
   });
