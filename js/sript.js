e24b7d3dd592ec6ccb495dbbbe63e7e7e1b06c13
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-11 22:53:00 +0000

    update CSS and script.js

--- js/sript.js
+++ js/sript.js
@@ -2,22 +2,30 @@ var button = document.getElementById('addButton');
 var userInput = document.getElementById('userInput');
 var listItem = document.getElementById('listItem');
 
-button.addEventListener('click', function() {
-    if(userInput.value.length > 0){
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(userInput.value));
-        listItem.appendChild(li);
-        userInput.value = '';
-    }
-   
-  });
+function inputItem (){
+  return userInput.value.length;
+};
 
-  userInput.addEventListener('keypress', function(event) {
-    if(userInput.value.length > 0 && event.keyCode === 13 ){
-        var li = document.createElement('li');
+function addListItem(){
+  var li = document.createElement('li');
         li.appendChild(document.createTextNode(userInput.value));
         listItem.appendChild(li);
         userInput.value = '';
-    }
-   
-  });
+};
+
+function createOnkeypress (event){
+  if(inputItem() > 0 && event.keyCode === 13 ){
+    addListItem();
+}
+};
+
+function createOnClick(){
+  if(inputItem() > 0){
+    addListItem();
+}
+};
+
+
+button.addEventListener('click', createOnClick);
+
+userInput.addEventListener('keypress', createOnkeypress);
