def955fd87cd35f9738e4ad84d787c87c77a0c61
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-29 20:51:16 +0000

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -1,70 +1,93 @@
 window.addEventListener("load", () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
   const form = document.querySelector("#todoForm");
   const input = document.querySelector("#todoInput");
-  const todoList = document.querySelector("#todos");
 
+  const todo = input.value;
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const todo = input.value;
+    
+    
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    e.target.reset();
+    renderTodos();
+  })
+  renderTodos();
+})
 
-    if(!todo){
-      alert("please enter a task")
-    }
+
+
+function renderTodos(){
+  todos.forEach(todo => {
+    const todoList = document.createElement("section");
+    todoList.classList.add("todoList");
+
 
     const todoListContainer = document.createElement("div");
-    todoListContainer.classList.add("todo");
+    todoListContainer.classList.add("taskContainer");
 
-    const todoListItem = document.createElement("div");
-    todoListItem.classList.add("todoContent");
+  const todoListItem = document.createElement("div");
+  todoListItem.classList.add("todo");
 
-    todoListContainer.appendChild(todoListItem);
-    
-    const todoItem = document.createElement("input");
-    todoItem.classList.add("todoInput");
-    todoItem.type = "text";
-    todoItem.value = todo;
-    todoItem.setAttribute("readonly", "readonly");
+  todoListContainer.appendChild(todoListItem); 
 
-    todoListItem.appendChild(todoItem);
-
-    const actionButtons = document.createElement("div");
-    actionButtons.classList.add("actionButtons");
-
-    const editTodo = document.createElement("button");
-    editTodo.classList.add("editButton");
-    editTodo.innerHTML = "Edit";
-
-    const removeTodo = document.createElement("button");
-    removeTodo.classList.add("removeButton");
-    removeTodo.innerHTML = "Remove";
-
-    actionButtons.appendChild(editTodo);
-    actionButtons.appendChild(removeTodo);
-
-    todoListContainer.appendChild(actionButtons); 
-
-    todoList.appendChild(todoListContainer); 
-
-    input.value = "";
-
-    editTodo.addEventListener("click", ()=>{
-      if(editTodo.innerText === "EDIT"){
-        todoItem.removeAttribute("readonly");
-        todoItem.focus();
-        editTodo.innerText = "Save";
-      } else{
-        todoItem.setAttribute("readonly", "readonly");
-        editTodo.innerText = "EDIT";                
-      }
-    })
+  const todoContent = document.createElement("div");
+  todoContent.classList.add("todoContent");
+  
+  const todoItem = document.createElement("input");
+  todoItem.classList.add("todoInput");
+  todoItem.type = "text";
+  todoItem.value = todo;
+  todoItem.setAttribute("readonly", "readonly");
 
 
-    removeTodo.addEventListener("click", () => {
-      todoList.removeChild(todoListContainer);
-    })
+  const actionButtons = document.createElement("div");
+  actionButtons.classList.add("actionButtons");
+
+  const editTodo = document.createElement("button");
+  editTodo.classList.add("editButton");
+  editTodo.innerHTML = "Edit";
+
+  const removeTodo = document.createElement("button");
+  removeTodo.classList.add("removeButton");
+  removeTodo.innerHTML = "Remove";
+
+  actionButtons.appendChild(editTodo);
+  actionButtons.appendChild(removeTodo);
+  todoContent.appendChild(todoItem);
+  todoContent.appendChild(actionButtons);
+  todoListItem.appendChild(todoContent);
+  
 
 
+  todoList.appendChild(todoListContainer); 
+
+
+  editTodo.addEventListener("click", ()=>{
+    if(editTodo.innerText === "EDIT"){
+      todoItem.removeAttribute("readonly");
+      todoItem.focus();
+      editTodo.innerText = "Save";
+      localStorage.setItem("todos", JSON.stringify(todos));
+      renderTodos()
+    } else{
+      todoItem.setAttribute("readonly", "readonly");
+      editTodo.innerText = "EDIT"; 
+      localStorage.setItem("todos", JSON.stringify(todos));
+      renderTodos()              
+    }
+    renderTodos();
   })
-})
+
+
+  removeTodo.addEventListener("click", () => {
+    todoList.removeChild(todoListContainer);
+    localStorage.setItem("todos", JSON.stringify(todos));
+      renderTodos()
+  })
+  })
+
+}
